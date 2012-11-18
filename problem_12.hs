1cc2494d33d142894025f2a37f44c92d9716a374
hekt <hektorg@gmail.com>
2012-11-18 22:50:43 +0900

    Use MyMath module

--- problem_12.hs
+++ problem_12.hs
@@ -1,6 +1,8 @@
 -- Project Euler Problem 12
 -- What is the first triangular number that has more than 501 divisors.
 
+import MyMath
+
 main = print $ sieve 501
 
 sieve :: Int -> Int
@@ -23,19 +25,3 @@ countWhile :: Eq a => [a] -> [Int]
 countWhile [] = []
 countWhile xxs@(x:_) = (length $ takeWhile (==x) xxs) : 
                        (countWhile $ dropWhile (==x) xxs)
-
-primes :: [Int]
-primes = filter isPrime [0..]
-
-isPrime :: Int -> Bool
-isPrime 0 = False
-isPrime 1 = False
-isPrime 2 = True
-isPrime n = ip (2: [3, 5 .. (isqrt n)])
-    where ip [] = True
-          ip (x:xs)
-              | n `mod` x == 0 = False
-              | otherwise = ip xs
-
-isqrt :: Int -> Int
-isqrt = floor . sqrt . fromIntegral
