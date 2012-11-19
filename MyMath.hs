499ab3384ccea375592fd002a67b093bff69c675
hekt <hektorg@gmail.com>
2012-11-19 19:50:42 +0900

    Improve type declarations
    Modify to use Integeral class from Int type

--- MyMath.hs
+++ MyMath.hs
@@ -1,16 +1,21 @@
 module MyMath
     ( isqrt
+    , fact
     , primes
     , isPrime
     ) where
 
-isqrt :: Int -> Int
+isqrt :: Integral a => a -> a
 isqrt = floor . sqrt . fromIntegral
 
-primes :: [Int]
+fact :: Integral a => a -> a
+fact 0 = 1
+fact n = n * fact (n-1)
+
+primes :: Integral a => [a]
 primes = filter isPrime [0..]
 
-isPrime :: Int -> Bool
+isPrime :: Integral a => a -> Bool
 isPrime 0 = False
 isPrime 1 = False
 isPrime 2 = True
