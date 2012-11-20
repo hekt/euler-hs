a50afbc41e21ad1973f822cf5706c425cf26d20f
hekt <hektorg@gmail.com>
2012-11-21 01:06:25 +0900

    Rename the function

--- problem_21.hs
+++ problem_21.hs
@@ -13,11 +13,11 @@ main = print $ solve 9999
 solve :: Int -> Int
 solve n = sum $ amicables n
 
-sumOfDivs' :: Int -> Int
-sumOfDivs' n = 1 + sum [ x | x <- [2 .. (n `div` 2)]
+sumOfDivs :: Int -> Int
+sumOfDivs n = 1 + sum [ x | x <- [2 .. (n `div` 2)]
                        , n `mod` x == 0]
 
 amicables :: Int -> [Int]
 amicables n = [ x | x <- [1..n] , let x' = ary ! x
               , x /= x', n >= x' , x == (ary ! x') ]
-    where ary = listArray (1, n) [sumOfDivs' x | x <- [1..n]]
+    where ary = listArray (1, n) [sumOfDivs x | x <- [1..n]]
