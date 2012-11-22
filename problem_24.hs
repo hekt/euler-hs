70b556bb5f75fe1bb44e4c5218f2bf4c75c58c31
hekt <hektorg@gmail.com>
2012-11-23 00:39:18 +0900

    Rename functions

--- problem_24.hs
+++ problem_24.hs
@@ -11,7 +11,7 @@ import Data.List (sort)
 main = print . concat . map show $ solve (10^6) [0..9]
 
 solve :: Int -> [Int] -> [Int]
-solve n xs = nthComb n xs
+solve n xs = nthPerm n xs
 
 pop :: Int -> [a] -> (a, [a])
 pop n xs = ((xs !! n'), f 1 xs)
@@ -21,14 +21,14 @@ pop n xs = ((xs !! n'), f 1 xs)
               | c == n    = tail yys
               | otherwise = y : f (c+1) ys
 
-nthComb :: Ord a => Int -> [a] -> [a]
-nthComb n xs = nthComb' n $ sort xs
+nthPerm :: Ord a => Int -> [a] -> [a]
+nthPerm n xs = nthPerm' n $ sort xs
 
-nthComb' :: Int -> [a] -> [a]
-nthComb' _ [] = []
-nthComb' n xs 
+nthPerm' :: Int -> [a] -> [a]
+nthPerm' _ [] = []
+nthPerm' n xs 
     | r == 0         = y : reverse ys
-    | otherwise      = z : nthComb' r zs
+    | otherwise      = z : nthPerm' r zs
     where (q, r) = divMod n $ fact (length xs - 1)
           (y, ys) = pop q xs
           (z, zs) = pop (q+1) xs
