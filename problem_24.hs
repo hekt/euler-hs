53df597711bd82fa2691417a7994ca26fdde8035
hekt <hektorg@gmail.com>
2012-11-23 01:53:26 +0900

    Improve

--- problem_24.hs
+++ problem_24.hs
@@ -11,7 +11,7 @@ import Data.List (sort)
 main = print . concat . map show $ solve (10^6) [0..9]
 
 solve :: Int -> [Int] -> [Int]
-solve n xs = nthPerm n xs
+solve n xs = nthPerm n $ sort xs
 
 pop :: Int -> [a] -> (a, [a])
 pop n xs = (xs !! (n-1), f 1 xs)
@@ -20,14 +20,11 @@ pop n xs = (xs !! (n-1), f 1 xs)
               | c == n    = ys
               | otherwise = y : f (c+1) ys
 
-nthPerm :: Ord a => Int -> [a] -> [a]
-nthPerm n xs = nthPerm' n $ sort xs
-
-nthPerm' :: Int -> [a] -> [a]
-nthPerm' _ [] = []
-nthPerm' n xs 
+nthPerm :: Int -> [a] -> [a]
+nthPerm _ [] = []
+nthPerm n xs 
     | r == 0    = y : reverse ys
-    | otherwise = z : nthPerm' r zs
+    | otherwise = z : nthPerm r zs
     where (q, r) = divMod n $ fact (length xs - 1)
           (y, ys) = pop q xs
           (z, zs) = pop (q+1) xs
