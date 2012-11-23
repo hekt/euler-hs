cad5066aa4201223851574a4ad102b701ddb6ce7
hekt <hektorg@gmail.com>
2012-11-24 06:17:27 +0900

    Add maxBy, maximumBy'

--- MyList.hs
+++ MyList.hs
@@ -1,6 +1,18 @@
 module MyList
     ( pop
+    , maximumBy'
+    , maxBy
     ) where
+
+import Data.List
 
 pop :: Int -> [a] -> (a, [a])
 pop n xs = (xs !! n, take n xs ++ drop (n+1) xs)
+
+maximumBy' :: (a -> a -> Ordering) -> [a] -> a
+maximumBy' o xs = foldl1' (maxBy o) xs
+
+maxBy :: (a -> a -> Ordering) -> a -> a -> a
+maxBy o x y
+    | o x y == GT = x
+    | otherwise   = y
