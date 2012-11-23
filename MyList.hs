f6becd3f966a1709d940fee7d856c3ac16fc07ec
hekt <hektorg@gmail.com>
2012-11-24 06:22:17 +0900

    Modify

--- MyList.hs
+++ MyList.hs
@@ -10,9 +10,9 @@ pop :: Int -> [a] -> (a, [a])
 pop n xs = (xs !! n, take n xs ++ drop (n+1) xs)
 
 maximumBy' :: (a -> a -> Ordering) -> [a] -> a
-maximumBy' o xs = foldl1' (maxBy o) xs
+maximumBy' cmp xs = foldl1' (maxBy cmp) xs
 
 maxBy :: (a -> a -> Ordering) -> a -> a -> a
-maxBy o x y
-    | o x y == GT = x
-    | otherwise   = y
+maxBy cmp x y
+    | cmp x y == GT = x
+    | otherwise     = y
