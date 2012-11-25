7e353a37dda8318fd5415373ba89f26b3cb655c6
hekt <hektorg@gmail.com>
2012-11-26 02:34:09 +0900

    Add int2list, int2list'

--- MyList.hs
+++ MyList.hs
@@ -2,6 +2,8 @@ module MyList
     ( pop
     , maximumBy'
     , maxBy
+    , int2list
+    , int2list'
     ) where
 
 import Data.List
@@ -17,3 +19,11 @@ maxBy :: (a -> a -> Ordering) -> a -> a -> a
 maxBy cmp x y
     | cmp x y == GT = x
     | otherwise     = y
+
+int2list :: Int -> [Int]
+int2list n = reverse $ int2list' n
+
+int2list' :: Int -> [Int]
+int2list' n
+    | n < 10 = n : []
+    | otherwise = n `mod` 10 : int2list' (n `div` 10)
