6f40b460e57ae6701568a291e05dfb90869b5fc8
hekt <hektorg@gmail.com>
2012-11-26 17:38:54 +0900

    Add list2int, combs. Improve int2list, int2list'

--- MyList.hs
+++ MyList.hs
@@ -4,6 +4,8 @@ module MyList
     , maxBy
     , int2list
     , int2list'
+    , list2int
+    , combs
     ) where
 
 import Data.List
@@ -20,10 +22,19 @@ maxBy cmp x y
     | cmp x y == GT = x
     | otherwise     = y
 
-int2list :: Int -> [Int]
+int2list :: Integral a => a -> [a]
 int2list n = reverse $ int2list' n
 
-int2list' :: Int -> [Int]
+int2list' :: Integral a => a -> [a]
 int2list' n
     | n < 10 = n : []
     | otherwise = n `mod` 10 : int2list' (n `div` 10)
+
+list2int :: Integral a => [a] -> a
+list2int [] = 0
+list2int (n:ns) = n * 10 ^ length ns + list2int ns
+
+combs :: [a] -> Int -> [[a]]
+combs [] _ = []
+combs xs 1 = map (:[]) xs
+combs (x:xs) n = [ x:y | y <- combs xs (n-1) ] ++ combs xs n
