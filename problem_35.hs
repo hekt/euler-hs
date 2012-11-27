a2d4a6e51c5353138f1bd1058b7e245224671c10
hekt <hektorg@gmail.com>
2012-11-27 19:16:55 +0900

    Solve Problem 35
    $ time ./problem_35
    55
    
    real    0m0.145s
    user    0m0.109s
    sys     0m0.007s

---
+++ problem_35.hs
@@ -0,0 +1,27 @@
+{- 
+   Problem 35
+   How many circular primes are there below one million?
+   
+   http://projecteuler.net/problem=35
+ -}
+
+import MyMath (isPrime, primes)
+import MyList (list2int, int2list, repPerms)
+
+main = print $ length solve2
+
+solve1 :: [Int]
+solve1 = 2: 5: [ x | x <- primes', let x' = int2list x, f x', g x' ]
+    where primes' = takeWhile (< 10^6) primes
+          f ns = all (==False) $ map (flip elem ns) [2,4,5,6,8]
+          g ns = all isPrime . map list2int . tail $ rotates ns
+
+solve2 = [2]: [5]: filter f ps
+    where ps = concatMap (repPerms [1,3,7,9]) [1..6]
+          f ns = all isPrime . map list2int $ rotates ns
+
+rotate :: [a] -> [a]
+rotate (x:xs) = xs ++ [x]
+
+rotates :: [a] -> [[a]]
+rotates xs = scanl (\acc x -> x acc) xs $ replicate (length xs - 1) rotate
