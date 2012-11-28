0528f892bfcdf008f4637637473c65e3520363cd
hekt <hektorg@gmail.com>
2012-11-29 06:39:05 +0900

    Solve Problem 37
    $ time ./problem_37
    748317
    
    real    0m2.392s
    user    0m2.324s
    sys     0m0.042s

---
+++ problem_37.hs
@@ -0,0 +1,21 @@
+{-  
+    Problem 37
+    Find the sum of all eleven primes that are both truncatable from left to right and right to left.
+    
+    http://projecteuler.net/problem=37
+ -}
+
+import Data.List (inits, tails)
+import MyMath (primes, isPrime)
+import MyList (int2list, list2int)
+
+main = print $ sum solve
+
+solve = take 11 [ n | n <- primes', f $ int2list n ]
+    where primes' = dropWhile (< 10) primes
+          f ns = (all' . body $ inits ns) && (all' . body $ tails ns)
+          all' = all ((ips !!) . list2int)
+          ips = [isPrime n | n <- [0..]]
+
+body :: [a] -> [a]
+body = init . tail
