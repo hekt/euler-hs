d83bfcadb70ee65921d8f200c7105a737e6c6bc1
hekt <hektorg@gmail.com>
2012-12-16 15:53:17 +0900

    Solve Problem 59 in 1.088s
    $ time ./problem_59
    107359
    
    real    0m1.088s
    user    0m1.041s
    sys     0m0.015s

---
+++ problem_59.hs
@@ -0,0 +1,40 @@
+{-
+  Problem 59
+  Using a brute force attack, can you dctypt the cipher using XOR encryption?
+  
+  http://projecteuler.net/problem=59
+ -}
+
+import System.IO
+import Data.Char (ord, chr)
+import Data.Bits (xor)
+import Data.List (sort)
+import MyList (perms)
+
+main = do
+  withFile "inputs/problem_59.txt" ReadMode $ \handle -> do
+         cipher <- fmap formatting $ hGetContents handle
+         print $ solve cipher
+
+formatting :: String -> [Int]
+formatting s = read ("["++s++"]")
+
+solve :: [Int] -> Int
+solve s = sum $ brute s
+
+brute :: [Int] -> [Int]
+brute s = head . filter f $ map (code s) keys
+    where keys = perms [ord 'a' .. ord 'z'] 3
+          f x = all (flip elem (words $ map chr x)) ["the", "of", "and"]
+              where ws = words $ map chr x
+
+code :: [Int] -> [Int] -> [Int]
+code xs ks = zipWith xor xs (cycle ks)
+
+count :: [Int] -> [(Int, Int)]
+count ns = count' $ sort ns
+
+count' :: [Int] -> [(Int, Int)]
+count' [] = []
+count' ns = (len, head ns): count' (drop len ns)
+    where len = length $ takeWhile (==head ns) ns
