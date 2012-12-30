305083775ba792f5bc2d9bc4fd2bd4f5b4e86be1
hekt <hektorg@gmail.com>
2012-12-31 00:54:58 +0900

    Remove a unnecesary definition

--- problem_76.hs
+++ problem_76.hs
@@ -10,5 +10,4 @@ solve n = foldl f (1 : repeat 0) ([1..n-1]) !! n
     where 
       f acc x = let (a, cc) = splitAt x acc
                 in a ++ gen a cc
-      gen _      []     = []
       gen (m:ms) (n:ns) = let mn = m + n in mn : gen (ms++[mn]) ns
