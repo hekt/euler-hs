4801b5c1fcfac18e2d9c040dcdcbe120a0f626b0
hekt <hektorg@gmail.com>
2013-01-04 01:25:57 +0900

    Modify

--- problem_77.hs
+++ problem_77.hs
@@ -5,13 +5,13 @@
 
 import MyMath (primes)
 
-main = print $ solve (5000-1)
+main = print $ solve 5000
 
 solve :: Int -> Int
 solve n = f (1:repeat 0) primes 2
     where
       f xs (p:ps) c = let (x, s) = splitAt p xs
-                          xs' = x ++ g x s
-                      in if xs' !! c > n then c
+                          xs'    = x ++ g x s
+                      in if xs' !! c >= n then c
                          else f xs' ps (c+1)
       g (m:ms) (n:ns) = let mn = m + n in mn : g (ms++[mn]) ns
